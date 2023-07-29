bac536e1f7f6dd19bebc027ac45b43d5f4e80c11
ziontee113 <ziontee113@gmail.com>
2023-07-30 04:02:30 +0700

    Change bg to stone-900

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-slate-900 text-gray-200 flex flex-col items-center justify-center">
+    <main className="w-screen h-screen bg-stone-900 text-gray-200 flex flex-col items-center justify-center">
       <h1 className="text-9xl">Hello World</h1>
     </main>
   );
